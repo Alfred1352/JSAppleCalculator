82079ad2881939c330b64fdb1ca07a91ed8b1db3
Alfred1352 <penarandaalfred@outlook.com>
2023-06-14 17:47:45 -0700

    Highlights any op used, no longer shows on display

--- index.js
+++ index.js
@@ -1,15 +1,71 @@
+let displayValue = '';
+let operator = '';
+
+function appendNumber(number) {
+  let display = document.getElementById('display');
+  display.value += number;
+}
+
+function setOperator(opButton) {
+  let display = document.getElementById('display');
+  displayValue = parseFloat(display.value);
+  operator = opButton.value;
+  display.value = display.value;
+
+  let operatorButtons = document.getElementsByClassName('operator');
+  for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].classList.remove('active');
+  }
+
+  opButton.classList.add('active');
+
+  if (operator.toLowerCase() === 'x') {
+    operator = '*';
+  } else if (operator.toLowerCase() === '÷') {
+    operator = '/';
+  } else {
+    operator = opButton.value;
+  }
+}
+
 function calculate() {
   let display = document.getElementById('display');
-  display.value = eval(display.value);
+  let currentValue = parseFloat(display.value);
+
+  let result;
+
+  if (operator === '+') {
+    result = displayValue + currentValue;
+  } else if (operator === '-') {
+    result = displayValue - currentValue;
+  } else if (operator === '*') {
+    result = displayValue * currentValue;
+  } else if (operator === '/') {
+    result = displayValue / currentValue;
+  }
+
+  display.value = result;
+}
+
+function clearAll() {
+  let display = document.getElementById('display');
+  display.value = '';
+  displayValue = '';
+  operator = '';
+
+  let operatorButtons = document.getElementsByClassName('operator');
+  for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].classList.remove('active');
+  }
 }
 
 function toggleSign() {
   let display = document.getElementById('display');
   let currentValue = parseFloat(display.value);
 
-  let newValue = -currentValue
+  let newValue = -currentValue;
 
-  display.value = newValue
+  display.value = newValue;
 }
 
 function calculatePercentage() {
@@ -18,49 +74,5 @@ function calculatePercentage() {
 
   let percentageValue = currentValue / 100;
 
-  display.value = percentageValue; 
+  display.value = percentageValue;
 }
-
-let isInputEntered = false;
-let displayValue = '';
-
-function updateDisplay(value) {
-  let display = document.getElementById('display');
-  display.value = value;
-} 
-
-function clearAll() {
-  let display = document.getElementById('display');
-  display.value = '';
-
-  isInputEntered = false;
-  document.getElementById('clearButton').value = 'AC';
-}
-
-function clearLastInput() {
-  let display = document.getElementById('display');
-  let currentValue = display.value;
-  if (currentValue.length > 0) {
-    let lastCharacter = currentValue[currentValue.length - 1];
-    let regex = /[0-9+\-*/.%]/;
-  
-    if (regex.test(lastCharacter)) {
-    display.value = currentValue.slice(0, -1);
-    }
-  }
-}
-
-function handleInput() {
-  let display = document.getElementById('display');
-  let currentValue = display.value;
-
-  if (currentValue.length === 0) {
-    document.getElementById('clearButton').value = 'AC';
-  } else if (currentValue === displayValue) {
-    clearAll();
-  } else {
-    document.getElementById('clearButton').value = 'C';
-  }
-}
-
-document.addEventListener('click', handleInput)
